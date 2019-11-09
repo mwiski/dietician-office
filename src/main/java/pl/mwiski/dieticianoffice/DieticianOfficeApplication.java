3995b7b1092bb36c50ea2fbe1ebbfaf016873107
mwiski <wiskim1@gmail.com>
2019-11-09 13:22:24 +0100

    DIET: initial commit

---
+++ src/main/java/pl/mwiski/dieticianoffice/DieticianOfficeApplication.java
@@ -0,0 +1,13 @@
+package pl.mwiski.dieticianoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DieticianOfficeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DieticianOfficeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/mwiski/dieticianoffice/DieticianOfficeApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mwiski.dieticianoffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DieticianOfficeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
